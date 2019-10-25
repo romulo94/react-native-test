d43c1cd71f68e7ebe7dcb0117210c9610ca529b4
Romulo Rocha <romulorocha063@gmail.com>
2019-10-25 19:12:48 -0300

    more tests

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -4,11 +4,15 @@ import { render, fireEvent } from '@testing-library/react-native';
 import TechList from '~/components/TechList';
 
 describe('TechList', () => {
+  beforeAll(() => {
+    jest.mock('@react-native-community/async-storage');
+  });
   it('should be able to add new tech', () => {
     const { getByText, getByTestId } = render(<TechList />);
 
     fireEvent.changeText(getByTestId('tech-input'), 'NodeJs');
     fireEvent.press(getByText('Adicionar'));
+
 
     expect(getByText('NodeJs')).toBeTruthy();
     expect(getByTestId('tech-input')).toHaveProp('value', '');
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import {
   View, TextInput, TouchableOpacity, Text, FlatList,
 } from 'react-native';
@@ -8,6 +9,17 @@ import {
 export default function TechList() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
+
+  useEffect(async () => {
+    const response = await AsyncStorage.getItem('@rn-testv1');
+
+    if (response) setTechs(JSON.parse(response));
+  }, []);
+
+  useEffect(async () => {
+    await AsyncStorage.setItem('@rn-testv1', JSON.stringify(techs));
+  }, [techs]);
+
 
   function handleAdd() {
     setTechs([...techs, newTech]);
